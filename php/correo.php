fc6623d04b7282ab36ed901402296732f587c073
[DMatias <[Dbazanmatias2004@gmail.com>
2022-09-28 18:04:47 -0300

    Mandando el mail con destinatario, asunto y carta

--- php/correo.php
+++ php/correo.php
@@ -8,5 +8,11 @@
     $destinatario = "bazanmatias2004@gmail.com";
     $asunto = "Contacto desde nuestra web";
 
+    $carta = "De: $nombre \n";
+    $carta .= "De: $apellido \n";
+    $carta .= "Correo: $correo \n";
+    $carta .= "Mensaje: $mensaje \n";
 
+    //Enviar con mail los datos
+    mail($destinatario, $asunto, $carta);
 ?>
