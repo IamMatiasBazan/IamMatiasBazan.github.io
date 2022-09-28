96c80f7e9e9a4feb94b92f1424190094c4f0f960
[DMatias <[Dbazanmatias2004@gmail.com>
2022-09-28 17:53:36 -0300

    Preparando datos para el correo

--- php/correo.php
+++ php/correo.php
@@ -3,4 +3,10 @@
     $apellido = $_POST['apellido'];
     $correo = $_POST['correo'];
     $mensaje = $_POST['mensaje'];
+
+    //Datos del correo
+    $destinatario = "bazanmatias2004@gmail.com";
+    $asunto = "Contacto desde nuestra web";
+
+
 ?>
