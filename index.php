abbacac442dffb7cbf3b5ea544f8ab4021d3761e
[DMatias <[Dbazanmatias2004@gmail.com>
2022-10-01 12:45:19 -0300

    Agregando clases al boton visitar sitio del model y dandole un Margin

--- index.php
+++ index.php
@@ -213,18 +213,23 @@
                         <div class="container">
                             <div class="row justify-content-center">
                                 <div class="col-lg-8">
-                                    <h2 class="portfolio-modal-title text-secondary text-uppercase mb-0">Log Cabin</h2>
+                                    <h2 class="portfolio-modal-title text-secondary text-uppercase mb-0">Workshop</h2>
                                     <div class="divider-custom">
                                         <div class="divider-custom-line"></div>
                                         <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
                                         <div class="divider-custom-line"></div>
                                     </div>
                                     <img class="img-fluid rounded mb-5" src="assets/img/portfolio/cabin.png" alt="" />
-                                    <p class="mb-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia neque assumenda ipsam nihil, molestias magnam, recusandae quos quis inventore quisquam velit asperiores, vitae? Reprehenderit soluta, eos quod consequuntur itaque. Nam.</p>
-                                    <button class="btn btn-primary" data-bs-dismiss="modal">
-                                        <i class="fas fa-xmark fa-fw"></i>
-                                        Close Window
-                                    </button>
+                                    <div class="botones-model">
+                                        <button class="btn btn-primary btn-visitar-sitio" data-bs-dismiss="modal">
+                                        <i class="fa-sharp fa-solid fa-share-from-square"></i>
+                                            Visitar sitio
+                                        </button>
+                                        <button class="btn btn-primary" data-bs-dismiss="modal">
+                                        <i class="fa-sharp fa-solid fa-share-from-square"></i>
+                                            Visitar sitio
+                                        </button>
+                                    </div>
                                 </div>
                             </div>
                         </div>
@@ -367,6 +372,11 @@
                 </div>
             </div>
         </div>
+        <style>
+            .btn-visitar-sitio {
+                margin: 20px;
+            }
+        </style>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
         <script src="js/scripts.js"></script>
         <script src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script>
